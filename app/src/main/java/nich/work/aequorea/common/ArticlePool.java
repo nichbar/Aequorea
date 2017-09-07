470163f5a6d87eab79e04f37eb2bad167abf9502
nich <cnichbar@gmail.com>
2017-09-07 23:53:57 +0800

    Added article memory cache.

---
+++ app/src/main/java/nich/work/aequorea/common/ArticlePool.java
@@ -0,0 +1,33 @@
+package nich.work.aequorea.common;
+
+import android.util.LruCache;
+
+public class ArticlePool {
+    private static final int MAX_CACHED_ARTICLE_SIZE = 20;
+    
+    private final LruCache<Long, String> mCache;
+    
+    private ArticlePool() {
+        this.mCache = new LruCache<>(MAX_CACHED_ARTICLE_SIZE);
+    }
+    
+    private static class ArticlePoolHolder {
+        static final ArticlePool ARTICLE_CACHE_POOL = new ArticlePool();
+    }
+    
+    public static ArticlePool getCache(){
+        return ArticlePoolHolder.ARTICLE_CACHE_POOL;
+    }
+    
+    public void cache(long id, String articleData){
+        mCache.put(id, articleData);
+    }
+    
+    public String loadCache(long id) {
+        return mCache.get(id);
+    }
+    
+    public void recycle() {
+        mCache.evictAll();
+    }
+}
--- app/src/main/java/nich/work/aequorea/main/presenter/ArticlePresenter.java
+++ app/src/main/java/nich/work/aequorea/main/presenter/ArticlePresenter.java
@@ -1,6 +1,9 @@
 package nich.work.aequorea.main.presenter;
 
 import android.graphics.Bitmap;
+import android.text.TextUtils;
+
+import com.google.gson.Gson;
 
 import io.reactivex.Single;
 import io.reactivex.SingleEmitter;
@@ -9,6 +12,7 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
 import io.reactivex.schedulers.Schedulers;
+import nich.work.aequorea.common.ArticlePool;
 import nich.work.aequorea.common.network.NetworkService;
 import nich.work.aequorea.common.network.RequestManager;
 import nich.work.aequorea.common.presenter.BasePresenter;
@@ -18,8 +22,6 @@ import nich.work.aequorea.main.model.mainpage.Data;
 import nich.work.aequorea.main.ui.view.ArticleView;
 
 public class ArticlePresenter extends BasePresenter<ArticleView> {
-    private Article mArticle;
-    private Throwable mThrowable;
     private NetworkService mService;
     
     @Override
@@ -27,70 +29,87 @@ public class ArticlePresenter extends BasePresenter<ArticleView> {
         mService = RequestManager.getInstance().getRetrofit().create(NetworkService.class);
     }
     
-    public void load(long id) {
+    public void load(final long id) {
+        String cacheContent = ArticlePool.getCache().loadCache(id);
+        
+        if (!TextUtils.isEmpty(cacheContent)) {
+            Gson gson = new Gson();
+            Article article = gson.fromJson(cacheContent, Article.class);
+            
+            if (article != null) {
+                onArticleLoaded(article);
+                return;
+            }
+        }
+        
         mComposite.add(mService.getArticleDetailInfo(id)
-                .subscribeOn(Schedulers.newThread())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<Article>() {
-                    @Override
-                    public void accept(Article article) throws Exception {
-                        mArticle = article;
-                        publish();
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Exception {
-                        mThrowable = throwable;
-                        publish();
-                    }
-                })
-        );
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Consumer<Article>() {
+                @Override
+                public void accept(Article article) throws Exception {
+                    cacheArticle(id, article);
+                    onArticleLoaded(article);
+                }
+            }, new Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable throwable) throws Exception {
+                    onArticleError(throwable);
+                }
+            }));
     }
     
-    public void loadRecommendedArticles(long id){
+    public void loadRecommendedArticles(long id) {
         mComposite.add(mService.getRecommendedArticle(id)
-        .subscribeOn(Schedulers.newThread())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Consumer<Data>() {
-            @Override
-            public void accept(Data data) throws Exception {
-                mBaseView.onRecommendationLoaded(data.getData());
-            }
-        }, new Consumer<Throwable>() {
-            @Override
-            public void accept(Throwable throwable) throws Exception {
-                mBaseView.onRecommendationError(throwable);
-            }
-        }));
+            .subscribeOn(Schedulers.newThread())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Consumer<Data>() {
+                @Override
+                public void accept(Data data) throws Exception {
+                    mBaseView.onRecommendationLoaded(data.getData());
+                }
+            }, new Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable throwable) throws Exception {
+                    mBaseView.onRecommendationError(throwable);
+                }
+            }));
     }
     
-    public void startSaveArticleToStorage(final Bitmap bitmap, final String title){
+    public void startSaveArticleToStorage(final Bitmap bitmap, final String title) {
         mComposite.add(Single.create(new SingleOnSubscribe<String>() {
             @Override
             public void subscribe(@NonNull SingleEmitter<String> e) throws Exception {
                 String address = IOUtils.saveBitmapToExternalStorage(bitmap, title);
                 e.onSuccess(address);
-            }})
-        .subscribeOn(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .subscribe(new Consumer<String>() {
-            @Override
-            public void accept(String s) throws Exception {
-                mBaseView.onArticleSavedAsPictureSucceeded(s);
             }
-        }, new Consumer<Throwable>() {
-            @Override
-            public void accept(Throwable throwable) throws Exception {
-                mBaseView.onArticleSavedAsPictureFailed(throwable);
-            }
-        }));
+        })
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(new Consumer<String>() {
+                @Override
+                public void accept(String s) throws Exception {
+                    mBaseView.onArticleSavedAsPictureSucceeded(s);
+                }
+            }, new Consumer<Throwable>() {
+                @Override
+                public void accept(Throwable throwable) throws Exception {
+                    mBaseView.onArticleSavedAsPictureFailed(throwable);
+                }
+            }));
     }
     
-    private void publish() {
-        if (mArticle != null) {
-            mBaseView.onArticleLoaded(mArticle.getData());
-        } else if (mThrowable != null) {
-            mBaseView.onArticleError(mThrowable);
-        }
+    private void onArticleLoaded(Article article) {
+        mBaseView.onArticleLoaded(article.getData());
+    }
+    
+    private void onArticleError(Throwable throwable) {
+        mBaseView.onArticleError(throwable);
+    }
+    
+    private void cacheArticle(long id, Article article) {
+        Gson gson = new Gson();
+        String json = gson.toJson(article);
+        ArticlePool.getCache().cache(id, json);
     }
 }

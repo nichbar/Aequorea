ca0eefcc3b81661599f826d8f7d3e9a52f7300bf
nich <cnichbar@gmail.com>
2020-01-18 14:13:32 +0800

    Modify adaptive icon

--- app/src/main/java/nich/work/aequorea/common/network/NetworkConstants.java
+++ app/src/main/java/nich/work/aequorea/common/network/NetworkConstants.java
@@ -1,6 +1,5 @@
 package nich.work.aequorea.common.network;
 
-public class NetworkConstants {
-    public final static String CBNWEEK_HTTPS_HOST = "https://api.cbnweek.com";
-    public final static String CBNWEEK_HTTP_HOST = "http://api.cbnweek.com";
+class NetworkConstants {
+    final static String CBNWEEK_HTTPS_HOST = "https://api.cbnweek.com";
 }
--- app/src/main/java/nich/work/aequorea/common/ui/fragment/BaseFragment.java
+++
@@ -1,7 +0,0 @@
-package nich.work.aequorea.common.ui.fragment;
-
-import androidx.fragment.app.Fragment;
-
-public class BaseFragment extends Fragment {
-
-}

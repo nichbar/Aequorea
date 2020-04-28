9d760abba1b1fb3d7e358ca0c4651afd75a1bef8
nich <cnichbar@gmail.com>
2020-04-28 23:12:30 +0800

    Add flurry

--- app/src/main/java/nich/work/aequorea/Aequorea.java
+++ app/src/main/java/nich/work/aequorea/Aequorea.java
@@ -1,7 +1,10 @@
 package nich.work.aequorea;
 
 import android.app.Application;
+import android.util.Log;
 
+import com.flurry.android.FlurryAgent;
+import com.flurry.android.FlurryPerformance;
 import com.zzhoujay.richtext.RichText;
 
 import java.io.File;
@@ -26,6 +29,17 @@ public class Aequorea extends Application {
         mExecutor = Executors.newCachedThreadPool();
     
         initCache();
+        initFlurry();
+    }
+
+    private void initFlurry() {
+        new FlurryAgent.Builder()
+                .withDataSaleOptOut(false) //CCPA - the default value is false
+                .withCaptureUncaughtExceptions(true)
+                .withIncludeBackgroundSessionsInMetrics(true)
+                .withLogLevel(Log.VERBOSE)
+                .withPerformanceMetrics(FlurryPerformance.ALL)
+                .build(this, "CXH6FJ9VS4P8K3K8BFN9");
     }
     
     private void initCache() {
